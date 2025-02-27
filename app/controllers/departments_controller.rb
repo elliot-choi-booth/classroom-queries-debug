12c1ceccce554771b84778fe423b15c83f044f91
elliot-choi-booth <elliotc0@chicagobooth.edu>
2025-02-27 03:37:15 +0000

    fixed depts

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -7,7 +7,9 @@ class DepartmentsController < ApplicationController
 
   def show
     the_id = params.fetch("path_id")
-    @department = Department.where({:id => the_id })
+    matching_department = Department.all.where({:id => the_id })
+
+    @department = matching_department.at(0)
 
     render({ :template => "departments/show" })
   end
